fc7e2560274fcd35426a3ecade6d68a7b6bf9110
averdugo <angel.verdugo.ext@idrica.com>
2023-03-15 13:15:38 +0100

    First commit

--- WcfService1/PersonaService.svc.cs
+++ WcfService1/PersonaService.svc.cs
@@ -10,7 +10,7 @@ using WcfService1.Modelo;
 namespace WcfService1
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
-    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración
     public class PersonaService : IPersona
     {
         /*
