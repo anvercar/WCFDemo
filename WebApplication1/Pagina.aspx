8daeef489044735bd60ffb4c9b0992634cea8a00
averdugo <angel.verdugo.ext@idrica.com>
2023-03-15 13:12:36 +0100

    Agregar archivos de proyecto.

---
+++ WebApplication1/Pagina.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Pagina.aspx.cs" Inherits="WebApplication1.Pagina" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="Grilla" runat="server">
+        </asp:GridView>
+        <asp:Button ID="btnConsultar" runat="server" OnClick="btnConsultar_Click" Text="Consultar" />
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Pagina.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1.ServiceReference1;
+
+namespace WebApplication1
+{
+    public partial class Pagina : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Consultar()
+        {
+            using (PersonaClient cliente = new PersonaClient())
+            {
+                Grilla.DataSource = cliente.Consultar();
+                Grilla.DataBind();
+            }
+        }
+
+        protected void btnConsultar_Click(object sender, EventArgs e)
+        {
+            Consultar();
+        }
+    }
+}
